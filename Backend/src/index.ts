b55894e34782587b1292ff2bd124a285b72d6e6c
Bambi <148939281+adorosis@users.noreply.github.com>
2025-05-31 08:53:36 +0700

    prisma-setup

--- Backend/src/index.ts
+++ Backend/src/index.ts
@@ -1,15 +1,42 @@
-import { serve } from '@hono/node-server'
-import { Hono } from 'hono'
+import { serve } from '@hono/node-server';
+import { Hono } from 'hono';
+import { PrismaClient } from './generated/prisma/index.js';
+import dotenv from 'dotenv';
+import { mainRouter } from './routes/index.route.ts';
+import { cors } from 'hono/cors';
+import { logger } from 'hono/logger';
+import { userRouter } from './routes/user.route.ts';
 
-const app = new Hono()
+dotenv.config();
+const app = new Hono();
+export const db = new PrismaClient();
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
-})
+app.use(
+  cors({
+    origin: 'http://localhost:8080',
+    allowMethods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    credentials: true,
+  })
+);
 
-serve({
-  fetch: app.fetch,
-  port: 3000
-}, (info) => {
-  console.log(`Server is running on http://localhost:${info.port}`)
-})
+app.use(logger());
+
+serve(
+  {
+    fetch: app.fetch,
+    port: 3000,
+  },
+  (info) => {
+    console.log(`Server is running on http://localhost:${info.port}`);
+  }
+);
+db.$connect()
+  .then(() => {
+    console.log('Connected to the database');
+  })
+  .catch((error) => {
+    console.error('Error connecting to the database:', error);
+  });
+
+app.route('', mainRouter);
+app.route('', userRouter);
